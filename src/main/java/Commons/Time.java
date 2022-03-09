4877004b241850e6e1115cfa84797c1635ff82b5
Planet <fabioruscica@hotmail.it>
2022-03-09 16:04:19 +0100

    Time class containing static helpers such as application start time, frame delta time and more.

---
+++ src/main/java/Commons/Time.java
@@ -0,0 +1,35 @@
+package Commons;
+
+public class Time {
+
+    // Returns passed time in seconds since start of application
+    public static float GetTimeSinceStartOfApplication(){ return (float)( (System.nanoTime() - ApplicationStartTime) * 1E-9); }
+
+    // Time this application was started
+    private static final float ApplicationStartTime = System.nanoTime();
+    private static double FrameBeginTime = 0.0f;
+    private static double FrameEndTime = 0.0f;
+
+    // The interval in seconds from the last frame to the current one
+    private static double _deltaTime = 0.0f;
+
+    public static float DeltaTime(){
+        return (float)_deltaTime;
+    }
+
+    public static double GetApplicationStartTime(){
+        return ApplicationStartTime;
+    }
+    
+    public static void BeginFrame(){
+        EndFrame();
+        // Delta is calculated at the beginning, this way it properly tracks time between last frame and the new one
+        _deltaTime = (FrameEndTime - FrameBeginTime) * 1E-9;
+
+        FrameEndTime = FrameBeginTime = System.nanoTime();
+    }
+
+    private static void EndFrame(){
+        FrameEndTime = System.nanoTime();
+    }
+}
