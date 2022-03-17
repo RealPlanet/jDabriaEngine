55b5eded4c32f03ec8355b1d723d3ab784ebc727
Planet <fabioruscica@hotmail.it>
2022-03-17 14:12:01 +0100

    Class naming convention fix.

--- src/main/java/JDabria/KeyListener.java
+++ src/main/java/JDabria/KeyListener.java
@@ -4,20 +4,20 @@ import org.jetbrains.annotations.NotNull;
 
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 
-public class keyListener {
-    private static keyListener instance;
+public class KeyListener {
+    private static KeyListener instance;
 
     //Amount of keybindings that GLFW utilizes, possibly wrong!
     private final boolean[] keyPressed = new boolean[350];
 
     //<editor-fold desc="Singleton">
-    private keyListener(){
+    private KeyListener(){
 
     }
 
-    public static @NotNull keyListener Get(){
+    public static @NotNull KeyListener Get(){
         if(instance == null){
-            instance = new keyListener();
+            instance = new KeyListener();
         }
         return instance;
     }
@@ -25,7 +25,7 @@ public class keyListener {
 
     //<editor-fold desc="Callbacks">
     public static void keyCallback(long window, int key, int scanCode, int action, int mods){
-        keyListener listener = Get();
+        KeyListener listener = Get();
 
         // Possible actions are GLFW_RELEASE and GLFW_PRESS
         listener.keyPressed[key] = action == GLFW_PRESS;
@@ -34,7 +34,7 @@ public class keyListener {
 
     //<editor-fold desc="Data access methods">
     public static boolean isKeyPressed(int keyCode){
-        keyListener listener = Get();
+        KeyListener listener = Get();
         return listener.keyPressed[keyCode];
     }
     //</editor-fold>
