6b587c4140231fe0f1e7eefcbe0678e5bb597676
Planet <fabioruscica@hotmail.it>
2022-03-17 14:14:56 +0100

    Removed unnecessary code.

--- src/main/java/JDabria/ECP/Components/FontRenderer.java
+++ src/main/java/JDabria/ECP/Components/FontRenderer.java
@@ -1,19 +1,6 @@
 package JDabria.ECP.Components;
 
 import JDabria.ECP.Component;
-import JDabria.ECP.Components.Sprite.SpriteRenderer;
 
 public class FontRenderer extends Component {
-
-    @Override
-    public void start(){
-        if(gameObject.getComponent(SpriteRenderer.class) != null){
-            System.out.println("Found Sprite Renderer!");
-        }
-    }
-
-    @Override
-    public void update() {
-
-    }
 }
