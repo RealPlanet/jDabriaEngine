e3760e205ca4753c570e1ced5e1210057985d315
Planet <fabioruscica@hotmail.it>
2022-03-18 14:19:34 +0100

    General refactor

--- src/main/java/commons/Color.java
+++ src/main/java/commons/Color.java
@@ -7,21 +7,21 @@ import java.beans.ConstructorProperties;
 
 
 public class Color{
-    public static final Color WHITE         = new Color(1,1,1,1);
-    public static final Color BLACK         = new Color(0,0,0,1);
-    public static final Color RED           = new Color(1,0,0,1);
-    public static final Color GREEN         = new Color(0,1,0,1);
-    public static final Color BLUE          = new Color(0,0,1,1);
+    //region Pre-made colors
+    public transient static final Color WHITE         = new Color(1,1,1,1);
+    public transient static final Color BLACK         = new Color(0,0,0,1);
+    public transient static final Color RED           = new Color(1,0,0,1);
+    public transient static final Color GREEN         = new Color(0,1,0,1);
+    public transient static final Color BLUE          = new Color(0,0,1,1);
+    public transient static final Color PURPLE        = new Color(0.75f, 0, 1, 1);
+    //endregion
 
+    //region Member variables
     private float Red, Green, Blue;
     private float Alpha = 1f;
+    //endregion
 
-    public void setColor(@NotNull Color newColor){
-        Red = newColor.Red;
-        Green = newColor.Green;
-        Blue = newColor.Blue;
-    }
-
+    //region Constructors
     @ConstructorProperties({"red", "green", "blue", "alpha"})
     public Color(float r, float g, float b, float a){
         if(r > 1.0f){
@@ -47,10 +47,20 @@ public class Color{
     }
 
     public Color(){}
+    //endregion
 
+    //region Getters
     public float getRed() { return Red; }
     public float getGreen() { return Green; }
     public float getBlue() { return Blue; }
     public float getAlpha() { return Alpha; }
+    //endregion
 
+    //region Setters
+    public void setColor(@NotNull Color newColor){
+        Red = newColor.Red;
+        Green = newColor.Green;
+        Blue = newColor.Blue;
+    }
+    //endregion
 }
