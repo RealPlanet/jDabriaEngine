9cf64f434fdfdd731dd0f69485c4ee949c566993
Planet <fabioruscica@hotmail.it>
2022-03-18 15:05:05 +0100

    Refactor

--- src/main/java/jDabria/ECP/Component.java
+++ src/main/java/jDabria/ECP/Component.java
@@ -1,11 +1,15 @@
 package jDabria.ECP;
 
-public abstract class Component {
-    public GameObject gameObject = null;
+public abstract class Component{
+    // This field is marked as transient to avoid a circular reference when serializing the component.
+    // This means that serializing a component will lose the game object reference!
+    public transient GameObject gameObject = null;
 
     public void start(){}
 
     public void stop(){}
 
     public void update(){}
+
+    public void delete(){}
 }
