f0a378ac17992a1e67a52ddada51da204fa2973e
Planet <fabioruscica@hotmail.it>
2022-03-22 10:35:35 +0100

    Refactored

--- src/main/java/commons/StringUtils.java
+++ src/main/java/commons/StringUtils.java
@@ -14,6 +14,7 @@ import java.util.Base64;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterOutputStream;
 
+@SuppressWarnings("unused")
 public class StringUtils {
     public static @NotNull String format(@Nullable String str, @Nullable Object... formats) {
         if (str == null) return "";
